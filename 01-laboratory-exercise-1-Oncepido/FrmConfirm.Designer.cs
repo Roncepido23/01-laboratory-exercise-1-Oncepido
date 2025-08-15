c73c6d658a10aa67350b7c5062d54b0abd4c2a3f
Roncepido23 <oncepidorobin34@gmail.com>
2025-08-15 17:15:54 +0800

    quite an error but have a confirm form and have code in it

--- 01-laboratory-exercise-1-Oncepido/FrmConfirm.Designer.cs
+++ 01-laboratory-exercise-1-Oncepido/FrmConfirm.Designer.cs
@@ -28,19 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1_StudentNumber = new System.Windows.Forms.Label();
+            this.label2_Program = new System.Windows.Forms.Label();
+            this.label3_LastName = new System.Windows.Forms.Label();
+            this.label4_FirstName = new System.Windows.Forms.Label();
+            this.label5_MiddleName = new System.Windows.Forms.Label();
+            this.label6_Age = new System.Windows.Forms.Label();
+            this.label7_ContactNumber = new System.Windows.Forms.Label();
+            this.label8_Address = new System.Windows.Forms.Label();
+            this.button1_Submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1_StudentNumber
+            // 
+            this.label1_StudentNumber.AutoSize = true;
+            this.label1_StudentNumber.Location = new System.Drawing.Point(21, 26);
+            this.label1_StudentNumber.Name = "label1_StudentNumber";
+            this.label1_StudentNumber.Size = new System.Drawing.Size(93, 13);
+            this.label1_StudentNumber.TabIndex = 0;
+            this.label1_StudentNumber.Text = "Student Number : ";
+            // 
+            // label2_Program
+            // 
+            this.label2_Program.AutoSize = true;
+            this.label2_Program.Location = new System.Drawing.Point(21, 85);
+            this.label2_Program.Name = "label2_Program";
+            this.label2_Program.Size = new System.Drawing.Size(55, 13);
+            this.label2_Program.TabIndex = 1;
+            this.label2_Program.Text = "Program : ";
+            // 
+            // label3_LastName
+            // 
+            this.label3_LastName.AutoSize = true;
+            this.label3_LastName.Location = new System.Drawing.Point(21, 158);
+            this.label3_LastName.Name = "label3_LastName";
+            this.label3_LastName.Size = new System.Drawing.Size(67, 13);
+            this.label3_LastName.TabIndex = 2;
+            this.label3_LastName.Text = "Last Name : ";
+            // 
+            // label4_FirstName
+            // 
+            this.label4_FirstName.AutoSize = true;
+            this.label4_FirstName.Location = new System.Drawing.Point(21, 195);
+            this.label4_FirstName.Name = "label4_FirstName";
+            this.label4_FirstName.Size = new System.Drawing.Size(66, 13);
+            this.label4_FirstName.TabIndex = 3;
+            this.label4_FirstName.Text = "First Name : ";
+            // 
+            // label5_MiddleName
+            // 
+            this.label5_MiddleName.AutoSize = true;
+            this.label5_MiddleName.Location = new System.Drawing.Point(21, 236);
+            this.label5_MiddleName.Name = "label5_MiddleName";
+            this.label5_MiddleName.Size = new System.Drawing.Size(78, 13);
+            this.label5_MiddleName.TabIndex = 4;
+            this.label5_MiddleName.Text = "Middle Name : ";
+            // 
+            // label6_Age
+            // 
+            this.label6_Age.AutoSize = true;
+            this.label6_Age.Location = new System.Drawing.Point(21, 305);
+            this.label6_Age.Name = "label6_Age";
+            this.label6_Age.Size = new System.Drawing.Size(35, 13);
+            this.label6_Age.TabIndex = 5;
+            this.label6_Age.Text = "Age : ";
+            // 
+            // label7_ContactNumber
+            // 
+            this.label7_ContactNumber.AutoSize = true;
+            this.label7_ContactNumber.Location = new System.Drawing.Point(21, 345);
+            this.label7_ContactNumber.Name = "label7_ContactNumber";
+            this.label7_ContactNumber.Size = new System.Drawing.Size(93, 13);
+            this.label7_ContactNumber.TabIndex = 6;
+            this.label7_ContactNumber.Text = "Contact Number : ";
+            // 
+            // label8_Address
+            // 
+            this.label8_Address.AutoSize = true;
+            this.label8_Address.Location = new System.Drawing.Point(21, 383);
+            this.label8_Address.Name = "label8_Address";
+            this.label8_Address.Size = new System.Drawing.Size(57, 13);
+            this.label8_Address.TabIndex = 7;
+            this.label8_Address.Text = "Address  : ";
+            // 
+            // button1_Submit
+            // 
+            this.button1_Submit.Location = new System.Drawing.Point(200, 473);
+            this.button1_Submit.Name = "button1_Submit";
+            this.button1_Submit.Size = new System.Drawing.Size(135, 34);
+            this.button1_Submit.TabIndex = 8;
+            this.button1_Submit.Text = "Submit";
+            this.button1_Submit.UseVisualStyleBackColor = true;
             // 
             // FrmConfirm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.button1_Submit);
+            this.Controls.Add(this.label8_Address);
+            this.Controls.Add(this.label7_ContactNumber);
+            this.Controls.Add(this.label6_Age);
+            this.Controls.Add(this.label5_MiddleName);
+            this.Controls.Add(this.label4_FirstName);
+            this.Controls.Add(this.label3_LastName);
+            this.Controls.Add(this.label2_Program);
+            this.Controls.Add(this.label1_StudentNumber);
             this.Name = "FrmConfirm";
             this.Text = "FrmConfirm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1_StudentNumber;
+        private System.Windows.Forms.Label label2_Program;
+        private System.Windows.Forms.Label label3_LastName;
+        private System.Windows.Forms.Label label4_FirstName;
+        private System.Windows.Forms.Label label5_MiddleName;
+        private System.Windows.Forms.Label label6_Age;
+        private System.Windows.Forms.Label label7_ContactNumber;
+        private System.Windows.Forms.Label label8_Address;
+        private System.Windows.Forms.Button button1_Submit;
     }
 }
--- 01-laboratory-exercise-1-Oncepido/FrmConfirm.cs
+++ 01-laboratory-exercise-1-Oncepido/FrmConfirm.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static _01_laboratory_exercise_1_Oncepido.StudentInfoClass;
 
 namespace _01_laboratory_exercise_1_Oncepido
 {
     public partial class FrmConfirm : Form
     {
+
+        private DelegateText DelProgram, DelLastName, DelMiddleName, DelAddress;
+
+        private DelegateNumber DelNumAge, DelNumContactNo, DelStudNo;
+
         public FrmConfirm()
         {
             InitializeComponent();
         }
+
+
+        private void FrmConfirm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
+
